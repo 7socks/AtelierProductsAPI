78c8e5fa4ee97c9153294dd2b44db5ad71e18699
Elfric <elf72742@gmail.com>
2022-04-20 14:25:11 -0400

    database connection ip updated

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection();
+const connection = mongoose.createConnection('mongodb://localhost:27017/products');
 
 const productSchema = new mongoose.Schema({
   id: Number,
